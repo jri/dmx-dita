c2ca1db08b0daca09e3c84e2f9c5740eeb094807
Jörg Richter <jri@deepamehta.de>
2020-02-18 18:59:08 +0100

    Add config for production build

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const PLUGIN_URI = 'systems.dmx.dita'
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/main/js/plugin.js',
+  output: {
+    path: __dirname + '/src/main/resources/web',
+    filename: '[chunkhash].plugin.js',
+    chunkFilename: '[chunkhash].[name].js',
+    publicPath: '/' + PLUGIN_URI + '/',
+    library: '_' + PLUGIN_URI.replace(/[.-]/g, '_'),
+    libraryTarget: 'jsonp'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+  ]
+}
